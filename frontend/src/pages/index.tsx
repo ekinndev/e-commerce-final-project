4341429c5eba13b3b3cde04a738e1334703cd445
Metehan Akmese <metehanakmese@gmail.com>
2023-04-05 21:20:54 +0300

    update 2

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           Welcome to
           <a href="https://nextjs.org"> Next.js2!!!!!!!</a>
         </h1>
-        <h2>Hello World</h2>
+        <h2>Hello Mete</h2>
         <Link href="/" locale={router.locale === 'en' ? 'tr' : 'en'}>
           <button onClick={() => dispatch({ count: 10, type: ActionType.INCREMENT_COUNTER })} type="button">
             Deneme
