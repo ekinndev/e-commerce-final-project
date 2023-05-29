d93e231b1d360dad4171aa10ae1bae823bd2c8d8
Ekin <abaliogluekin@gmail.com>
2023-05-29 14:42:48 +0300

    Fix socket

--- backend/src/socket-connection.ts
+++ backend/src/socket-connection.ts
@@ -6,7 +6,14 @@ let socketServer: any = null;
 module.exports = (server: Server) => {
     if (socketServer) return socketServer;
 
-    socketServer = new SocketServer(server);
+    socketServer = new SocketServer(server, {
+        cors: {
+            origin:
+                process.env.NODE_ENV === 'development'
+                    ? 'http://localhost:3001'
+                    : 'https://e-commerce-final-penwtklslq-ew.a.run.app',
+        },
+    });
 
     socketServer.on('connection', (socket: Socket) => {
         console.log('Socket connected: ', socket.id);
