135f4ab20cb2480f37d8889bc6bd7daf8c34c9b9
Ekin <abaliogluekin@gmail.com>
2023-06-17 14:46:20 +0300

    Fix url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   withCredentials: true,
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.ecommerce.ekinn.dev',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://e-commerce-final-be-penwtklslq-ew.a.run.app',
 });
 
 export default apiClient;
