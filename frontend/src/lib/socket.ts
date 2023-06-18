9b4fc9b987a56c9d3dd190544171b83b0c6061fd
Ekin <abaliogluekin@gmail.com>
2023-06-18 15:31:30 +0300

    test

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === 'production'
-    ? 'ws://e-commerce-final-be-penwtklslq-ew.a.run.app/draw'
+    ? 'https://e-commerce-final-be-penwtklslq-ew.a.run.app/draw'
     : 'ws://localhost:3000/draw';
 
 export const socket = io(URL);
