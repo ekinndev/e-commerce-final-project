8562031b1e4a1fbc49c8da66f3306b828107825f
Ekin <abaliogluekin@gmail.com>
2023-06-18 15:25:25 +0300

    Fix url

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? '' : 'ws://localhost:3000/draw';
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'ws://e-commerce-final-be-penwtklslq-ew.a.run.app/draw'
+    : 'ws://localhost:3000/draw';
 
 export const socket = io(URL);
