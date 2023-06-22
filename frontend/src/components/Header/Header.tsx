a279340695c9d5a786cc55aa44d7a98ad73d7abe
Ekin <abaliogluekin@gmail.com>
2023-06-22 21:14:13 +0300

    Add draw link

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -99,15 +99,9 @@ const Header: FC = props => {
       </div>
       <nav className=" py-3 max-w-[1440px] mx-auto flex items-center justify-between sticky top-0">
         <div className="flex gap-x-4 items-center">
-          <div className="flex cursor-pointer select-none items-center gap-x-2 py-1 px-2 first:px-0 text-black">
-            <IconHamburger />
-            <span className="text-sm font-medium">All Category</span>
-          </div>
-          <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Hot offers</span>
-          <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Gift boxes</span>
-          <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Projects</span>
-          <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Menu item</span>
-          <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Help</span>
+          <Link href="/draw">
+            <span className="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium ">Hot offers</span>
+          </Link>
         </div>
       </nav>
     </header>
